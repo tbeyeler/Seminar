96f00ab58be21e1e2b9e280410e1d5615c59dab4
beytob00 <tobias.beyeler@gmail.com>
2019-05-28 21:05:30 +0200

    Added some files and implemented the wagesforhousework.txt to be displayed in the window.

--- view/MainWindowController.java
+++ view/MainWindowController.java
@@ -3,19 +3,37 @@ package view;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.io.*;
 
 public class MainWindowController {
 
-    @FXML
-    private Button closeButton;
+  @FXML private Button closeButton;
 
-    @FXML
-    void handleCloseButton() {
-        Platform.exit();
-    }
+  @FXML private Label textLabel;
 
   @FXML
-  void initialize() {}
+  void handleCloseButton() {
+    Platform.exit();
+  }
 
-
+  @FXML
+  void initialize() {
+    File file =
+        new File("C:\\Users\\tobia\\Google Drive\\Seminar\\resources\\wagesforhousework.txt");
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(file));
+      String text;
+      StringBuffer sb = new StringBuffer();
+      while ((text = reader.readLine()) != null) {
+        sb.append(text);
+      }
+      textLabel.setText(sb.toString());
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException ie) {
+      ie.printStackTrace();
+    }
+  }
 }
