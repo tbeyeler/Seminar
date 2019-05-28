e382792aebbd394625538508e6f15485c2297e22
tbeyeler <tobias.beyeler@stud.unibas.ch>
2019-05-28 18:56:02 +0200

    Now the "Close"-button works

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
 
     @Override
     public void start(Stage stage) throws Exception {
-
-
+        BorderPane borderPane = FXMLLoader.load(getClass().getResource("/view/MainWindowLayout.fxml"));
+        Scene scene = new Scene(borderPane);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
 
     }
 }
--- view/MainWindowController.java
+++ view/MainWindowController.java
@@ -1,4 +1,21 @@
 package view;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainWindowController {
+
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    void handleCloseButton() {
+        Platform.exit();
+    }
+
+  @FXML
+  void initialize() {}
+
+
 }
