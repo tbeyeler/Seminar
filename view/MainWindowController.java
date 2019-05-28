0bc62ad13e2a10e3f8fa90b99a67f59cdf05a02e
beytob00 <tobias.beyeler@gmail.com>
2019-05-28 22:13:48 +0200

    Everything is prepared now.

--- view/MainWindowController.java
+++ view/MainWindowController.java
@@ -3,19 +3,50 @@ package view;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+import java.io.*;
 
 public class MainWindowController {
 
-    @FXML
-    private Button closeButton;
+    @FXML private Button closeButton;
+
+    @FXML private TextArea textArea;
 
     @FXML
     void handleCloseButton() {
         Platform.exit();
     }
 
-  @FXML
-  void initialize() {}
+    @FXML
+    void initialize() {
+        File file =
+                new File("C:\\Users\\tobia\\Google Drive\\Seminar\\resources\\wagesforhousework.txt");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int character;
+            StringBuffer sb = new StringBuffer();
+            int count = 0;
+            while ((character = reader.read()) != -1) {
+                Character letter = (char) character;
+                if (count >= 85 && letter.toString().equals(" ")) {
+                    sb.append(System.lineSeparator());
+                    count = 0;
+                } else {
+                    sb.append((char) character);
+                }
+                count++;
+            }
 
+            if (sb.length() > 0) {
+                textArea.setText(sb.toString());
+            }
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        }
+    }
 }
